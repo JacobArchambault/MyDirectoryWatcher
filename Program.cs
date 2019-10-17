2413a731d6a9199c78e2a07cffb6254252bcb24c
Jacob Archambault <jacob.archambault@waystar.com>
2019-10-17 19:21:29 -0400

    Complete project.

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace MyDirectoryWatcher
         static void Main(string[] args)
         {
             Console.WriteLine("***** The Amazing File Watcher App *****\n");
+
             // Establish the path to the directory to watch.
             FileSystemWatcher watcher = new FileSystemWatcher();
             try
@@ -25,36 +26,41 @@ namespace MyDirectoryWatcher
             }
             // Set up the things to be on the lookout for.
             watcher.NotifyFilter = NotifyFilters.LastAccess
-                | NotifyFilters.LastWrite
-                | NotifyFilters.FileName
-                | NotifyFilters.DirectoryName;
+              | NotifyFilters.LastWrite
+              | NotifyFilters.FileName
+              | NotifyFilters.DirectoryName;
 
             // Only watch text files.
             watcher.Filter = "*.txt";
 
-            // Add event handlers.
-            watcher.Changed += new FileSystemEventHandler(OnChanged);
-            watcher.Created += new FileSystemEventHandler(OnChanged);
-            watcher.Deleted += new FileSystemEventHandler(OnChanged);
-            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+            // Specify what is done when a file is changed, created, or deleted.
+            watcher.Changed += (s, e) =>
+            {
+                Console.WriteLine("File: {0} {1}!", e.FullPath, e.ChangeType);
+            };
+
+            watcher.Created += (s, e) =>
+            {
+                Console.WriteLine("File: {0} {1}!", e.FullPath, e.ChangeType);
+            };
+
+            watcher.Deleted += (s, e) =>
+            {
+                Console.WriteLine("File: {0} {1}!", e.FullPath, e.ChangeType);
+            };
+
+            watcher.Renamed += (s, e) =>
+            {
+                // Specify what is done when a file is renamed.
+                Console.WriteLine("File: {0} renamed to {1}", e.OldFullPath, e.FullPath);
+            };
 
             // Begin watching the directory.
             watcher.EnableRaisingEvents = true;
 
             // Wait for the user to quit the program.
             Console.WriteLine(@"Press 'q' to quit app.");
-            while (Console.Read() != 'q') 
-                ;
-        }
-        static void OnChanged(object source, FileSystemEventArgs e)
-        {
-            // Specify what is done when a file is changed, created, or deleted.
-            Console.WriteLine("File: {0} {1}!", e.FullPath, e.ChangeType);
-        }
-        static void OnRenamed(object source, RenamedEventArgs e)
-        {
-            // Specify what is done when a file is renamed.
-            Console.WriteLine("File: {0} renamed to {1}", e.OldFullPath, e.FullPath);
+            while (Console.Read() != 'q') ;
         }
     }
 }
